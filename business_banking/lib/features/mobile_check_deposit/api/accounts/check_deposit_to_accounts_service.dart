1dda20e2d739c9bb81082135cc4e3244bfe84b4d
satya <magamsatyam@gmail.com>
2021-02-22 10:39:35 -0500

    check deposit changes

--- business_banking/lib/features/mobile_check_deposit/api/accounts/check_deposit_to_accounts_service.dart
+++ business_banking/lib/features/mobile_check_deposit/api/accounts/check_deposit_to_accounts_service.dart
@@ -1,11 +1,11 @@
 import 'package:business_banking/features/mobile_check_deposit/api/accounts/check_deposit_to_accounts_response_model.dart';
-import 'package:business_banking/features/transfer_funds/api/accounts/transfer_accounts_to_request_model.dart';
-import 'package:business_banking/features/transfer_funds/api/accounts/transfer_accounts_to_response_model.dart';
 import 'package:business_banking/locator.dart';
 import 'package:clean_framework/clean_framework.dart';
 import 'package:clean_framework/clean_framework_defaults.dart';
 
-class CheckDepositToAccountService extends EitherService<TransferFundsAccountsToRequestModel, CheckDepositToAccountResponseModel> {
+import 'check_deposit_to_accounts_request_model.dart';
+
+class CheckDepositToAccountService extends EitherService<CheckDepositToAccountsRequestModel, CheckDepositToAccountResponseModel> {
   CheckDepositToAccountService() : super(
       method: RestMethod.post,
       restApi: ExampleLocator().api,
