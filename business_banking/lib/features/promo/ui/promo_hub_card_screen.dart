8bf026a60d75be3096e8da27c342fe1b5303701a
vladimir-str-huntington <vladimir.strigunov@huntington.com>
2021-06-24 13:51:42 -0700

    PromoHubCardScreen class with flutter UI of the feature added. Assets are all static and hard-coded

---
+++ business_banking/lib/features/promo/ui/promo_hub_card_screen.dart
@@ -0,0 +1,84 @@
+import 'package:clean_framework/clean_framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class PromoHubCardScreen extends Screen {
+  //TODO: implement below classes
+  //final PromoHubCardViewModel? viewModel;
+  //final PromoHubCardPresenterActions? actions;
+
+
+  PromoHubCardScreen();
+  //TODO: add to constructor:
+  //this.viewModel, this.actions
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        key: Key('adCard'),
+        color: Colors.white,
+        shadowColor: Colors.grey[500],
+        elevation: 3.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          child: Container(
+            color: Colors.white,
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Text(
+                              //TODO: pass viewmodel values:
+                              //viewModel!.promoValue,
+                            'Viewmodel text!',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18)),
+                        )),
+                    Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Image.network(
+                              //TODO: add promoImage
+                              //viewModel!.promoIconImage
+                              'http://www.w3.org/2000/svg',
+                            ),
+                        ))
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    key: Key('Ad-Card-Learn-More-Button'),
+                    child: Text(
+                      'Learn More',
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20),
+                    ),
+                    style: OutlinedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15)),
+                        side: BorderSide(width: 2, color: Colors.green)),
+                    onPressed: () {
+                      //TODO: add navigation after input has been validated
+                      //actions!.navigateToPromoCatalog(context);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
