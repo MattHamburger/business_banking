4ebe1f16b2d4c843eb0af6d7d649ffe70ab4f954
Ben Molina <ben@acme-software.com>
2021-07-13 11:35:39 -0400

    cashback feature initial files

---
+++ business_banking/lib/features/cashback/bloc/cashback_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:clean_framework/clean_framework.dart';
+
+class CashbackBloc extends Bloc {
+  @override
+  void dispose() {}
+}
---
+++ business_banking/lib/features/cashback/ui/cashback_hub_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// make this tapabble and navigate to the Feature Widget
+class CashbackHubWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text("Hello World");
+  }
+}
